d59fa0906c919b7829159ba90391c3bfa8c53b1c
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2020-04-02 11:25:25 +0200

    Upadting dom and looping thoruhg array

--- script.js
+++ script.js
@@ -30,4 +30,31 @@ async function getRandomUser() {
 function addData(obj) {
   //Push into array
   data.push(obj);
+
+  updateDOM();
 }
+
+//Update DOM
+function updateDOM(providedData = data) {
+  //Cleart main div
+  main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
+  //Looping
+  providedData.forEach(item => {
+    //Creating new element on documnet
+    const element = document.createElement("div");
+    //Adding the class on element
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+//Format number sa money
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&.");
+}
+
+//Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
